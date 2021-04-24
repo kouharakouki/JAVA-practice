3868eb4c37b0673c7e7819ec3d5c806d9b66b15d
koukikouhara <koki.kohara916@gmail.com>
2021-04-24 16:01:27 +0900

    Finish Chapter4

---
+++ src/Chapter04.java
@@ -0,0 +1,15 @@
+
+public class Chapter04 {
+
+	public static void main(String[] args) {
+		String name = "koki";
+		System.out.println(name);
+
+		name = "kokikoki";
+		System.out.println(name);
+
+		final int LUCKY_NUMBER = 7;
+		System.out.println(LUCKY_NUMBER);
+	}
+
+}
