8b577472356427557a081decb37d2d3cc2e31c6f
koukikouhara <koki.kohara916@gmail.com>
2021-04-25 10:30:44 +0900

    Finish Chapter5

---
+++ src/Chapter05.java
@@ -0,0 +1,24 @@
+
+public class Chapter05 {
+
+	public static void main(String[] args) {
+		long long1 = 1000L;
+		double double1 = 2.0;
+		boolean boolean1 = true;
+		System.out.println(long1);
+		System.out.println(double1);
+		System.out.println(boolean1);
+
+		Float float1 = 0.123f;
+		Integer int1 = 32;
+		Character cha1 = 'あ';
+		System.out.println(float1);
+		System.out.println(int1);
+		System.out.println(cha1);
+
+		int[] array1 = { 10, 20, 30, 40, 50 };
+		System.out.println(array1[3]);
+
+	}
+
+}
